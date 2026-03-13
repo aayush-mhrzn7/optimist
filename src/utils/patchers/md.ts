a1c972c5a5b7242d5600e1329d8f5a4b780852a4
aayush-mhrzn7 <aayushmaharjan497@gmail.com>
2026-03-13 19:10:35 +0545

    chore: boilterplate funcs

--- src/utils/patchers/md.ts
+++ src/utils/patchers/md.ts
@@ -1,1 +1,12 @@
 // START OF THE MD PARSER
+
+import  marked from 'marked'
+// this is the lexers that is going to get the getTokens
+function getTokens(){
+}
+
+function traverseTokens(){
+}
+
+function generateTheCodeBack(){
+}
